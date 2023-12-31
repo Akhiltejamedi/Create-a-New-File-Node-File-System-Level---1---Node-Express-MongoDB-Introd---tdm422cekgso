417834c8b9990dea84263dbd2089fb987ee2a9d9
Akhiltejamedi <akhilteja0123@gmail.com>
2023-12-31 19:07:54 +0530

    Your commit message here

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  try {
+    // Use fs.promises.writeFile to write the content to the file
+    await fs.writeFile(fileName, fileContent);
+    console.log(`${fileName} created successfully!`);
+  } catch (error) {
+    console.error(`Error creating ${fileName}:`, error);
+  }
 };
 
 module.exports = { writeFile };
+(async () => await writeFile(fileName, fileContent))();
